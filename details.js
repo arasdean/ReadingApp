55c1e5852fba3a101b1532d731f8fd752597f9f0
Aaron Dean <arasdean@Aarons-MacBook-Pro.local>
2017-10-19 11:06:23 -0700

    added .env details

--- details.js
+++ details.js
@@ -1,7 +1,10 @@
 const details = {
-  AccessId : "AKIAJCPGGNRYZ36LGAJA",
-  Secret: "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",
-  Tag: "readingappptt-20"
+  AccessId : process.env.ACCESSID,
+  Secret: process.env.SECRET,
+  Tag: process.env.TAG
+  // AccessId : "AKIAJCPGGNRYZ36LGAJA",
+  // Secret: "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",
+  // Tag: "readingappptt-20"
 }
 
 module.exports = details;
